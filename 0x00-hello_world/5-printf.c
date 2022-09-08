df377b8101a0581d27a3835b909060b0563ff3ac
alephkat <alphamuseka@gmail.com>
2022-09-08 14:54:20 -0700

    my commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,9 +4,10 @@
  * main-prints a text
  *
  * Return:Always 0 (sucess)
- * /
+ */
+ 
  int main(void)
  {
- printf("with proper grammar, but the outcome is a piece of art,\n");
- return(0);
+ 	printf("with proper grammar, but the outcome is a piece of art,\n");
+ 	return(0);
  }
