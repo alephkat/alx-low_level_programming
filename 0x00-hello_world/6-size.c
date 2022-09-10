11cfa85a85ce3a11ed91d705dede65839e7200f0
alephkat <alphamuseka@gmail.com>
2022-09-10 14:41:54 -0700

    my commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	long long int d;
 	float e;
 
-	printf("size of a char: %d byte(s)\n",sizeof(a));
-	printf("size of an init: %d byte(s)\n",sizeof(b));
-	printf("size of a long init: %d byte(s)\n",sizeof(c));
-	printf("size of a long long init: %d byte(s)\n",sizeof(d))
-	printf("size of a float: %d byte(s)\n",sizeof(e))
-	return(0)
+	printf("size of a char:%d byte(s)\n",sizeof(a));
+	printf("size of an init:%d byte(s)\n",sizeof(b));
+	printf("size of a long init:%d byte(s)\n",sizeof(c));
+	printf("size of a long long init:%d byte(s)\n",sizeof(d))
+	printf("size of a float:%d byte(s)\n",sizeof(e))
+	return(0);
 }	
