8254ab6cdef2737309de93f95f3057410d4b92ac
alephkat <alphamuseka@gmail.com>
2022-09-11 03:20:45 -0700

    my commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * main-Prints the size of many var types 
- * Return always 0 (sucess)
- */
+ *main - Prints quote
+ *Return: zero on success
+ **/
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
+	int charSize = sizeof(char);
+	int intSize = sizeof(int);
+	int longSize = sizeof(long int);
+	int longlongSize = sizeof(long long int);
+	int floatSize = sizeof(float);
 
-	printf("size of a char:%d byte(s)\n",sizeof(a));
-	printf("size of an init:%d byte(s)\n",sizeof(b));
-	printf("size of a long init:%d byte(s)\n",sizeof(c));
-	printf("size of a long long init:%d byte(s)\n",sizeof(d))
-	printf("size of a float:%d byte(s)\n",sizeof(e))
-	return(0);
-}	
+	printf("Size of a char: %d byte(s)\n", charSize);
+	printf("Size of an int: %d byte(s)\n", intSize);
+	printf("Size of a long int: %d byte(s)\n", longSize);
+	printf("Size of a long long int: %d byte(s)\n", longlongSize);
+	printf("Size of a float: %d byte(s)\n", floatSize);
+	return (0);
+}
