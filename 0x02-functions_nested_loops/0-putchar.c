4c88b813dfa8bfbd51ddf94409512f9ae8184b90
alephkat <alphamuseka@gmail.com>
2022-09-14 01:35:50 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * main-prints putchar
- * Return: always 0
+ *main - print putchar
+ *Return: always 0
  */
+
 int main(void)
+
 {
 	_putchar('_');
 	_putchar('p');
@@ -14,7 +16,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n'};
+	_putchar('\n');
 
 	return (0);
 }
