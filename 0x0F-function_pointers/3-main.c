84804de45946f990daa4d8f1ed7ecc0b87f45a10
alephkat <alphamuseka@gmail.com>
2022-10-13 16:04:19 -0700

    task3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,7 +19,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-				
+
 	num1 = atoi(argv[1]);
 	op = argv[2];
 	num2 = atoi(argv[3]);
@@ -38,6 +38,6 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	}
 
 	printf("%d\n", get_op_func(op)(num1, num2));
-	
+
 	return (0);
 }
